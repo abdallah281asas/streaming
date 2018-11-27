a8ed4ecc8504be2fae7c7fdcb7096cd4ffa02a53
abdallah281asas <45336899+abdallah281asas@users.noreply.github.com>
2018-11-28 01:18:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const prefix = '#';
 
 client.on('ready', () => {
 
- client.user.setGame(`GaMiKa Is Here`,'https://www.twitch.tv/MeeRcY')
+ client.user.setGame(`قالك صحاب!  كسم الصحاب`,'https://www.twitch.tv/MeeRcY')
  
  console.log(`Logged in as [ Vida]`);
 
